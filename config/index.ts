6baeaff04dfa56b52b75aad2bbbfa0e15cb82198
sujha <sujha@adobe.com>
2022-03-04 20:20:28 +0530

    fixing fetching 404

--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-export const server = dev ? 'http://localhost:3000' : 'https://subhashjha.in'
+export const server = dev ? 'http://localhost:3000' : 'https://subhashjha.in/rentreceipt'
--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  basePath: process.env.NODE_ENV !== 'production' ? '' : "/rentreceipt",
+  assetPrefix: process.env.NODE_ENV !== 'production' ? '' : "/rentreceipt"
 }
 
 module.exports = nextConfig
